02377b6e914072d10a06fe852671549283a2bc06
manishshrma <rsshrma14@hotmail.com>
2021-05-29 17:30:31 +0530

    spring
    123

--- src/main/java/com/example/SpringRedis/SpringRedisApplication.java
+++ src/main/java/com/example/SpringRedis/SpringRedisApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRedisApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringRedisApplication.class, args);
+
+		System.out.println("hello changes..");
+
+		//are you able to do it despite severe depression??
 	}
 
 }
